a30c614854f34e3090ddd457cb94f882365a0be3
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-10-26 16:27:17 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Олег Федоров on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
