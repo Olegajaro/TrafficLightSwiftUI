cb20570166a574a38ec478409d17ffbeaa6e0397
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-10-27 14:44:39 +0300

    First version

--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -12,44 +12,70 @@ struct ContentView: View {
         case red, yellow, green
     }
 
-    
     @State private var buttonName = "START"
-    @State private var lightIsOn = 1.0
+    @State private var lightIsOn = 5.0
     @State private var lightIsOff = 0.2
     @State private var currentLight = CurrentLight.red
-    
+    @State private var redColor: Color = .red
+    @State private var yellowColor: Color = .yellow
+    @State private var greenColor: Color = .green
+    @State private var tapCount = 0
     
     var body: some View {
         ZStack {
             Color(.black)
                 .ignoresSafeArea()
             VStack {
-                redSignal
+                TrafficSignalView(color: redColor)
                     .padding()
-                TrafficSignalView(color: .yellow.opacity(lightIsOff))
+                TrafficSignalView(color: yellowColor)
                     .padding(.bottom)
-                TrafficSignalView(color: .green.opacity(lightIsOff))
+                TrafficSignalView(color: greenColor)
                 Spacer()
-                Button {
-                    buttonName = "NEXT"
-                
-                } label: {
-                    Text(buttonName)
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                }
-                .padding()
-                .background(.blue)
-                .cornerRadius(20)
+                signalSwitchingButton
             }
+            .padding(.bottom)
         }
     }
     
-    private var redSignal: some View {
-        TrafficSignalView(color: .red.opacity(lightIsOff))
+    private var signalSwitchingButton: some View {
+        Button {
+            buttonName = "NEXT"
+            
+            tapCount += 1
+            
+            toggleTheSignal()
+        } label: {
+            Text(buttonName)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+        }
+        .frame(width: 150, height: 50)
+        .background(.blue)
+        .cornerRadius(20)
     }
-
+    
+    private func toggleTheSignal() {
+        switch currentLight {
+        case .red:
+            if tapCount == 1 {
+                greenColor = greenColor
+            } else {
+                greenColor = greenColor.opacity(lightIsOff)
+            }
+            redColor = redColor.opacity(lightIsOn)
+            currentLight = CurrentLight.yellow
+        case .yellow:
+            redColor = redColor.opacity(lightIsOff)
+            yellowColor = yellowColor.opacity(lightIsOn)
+            currentLight = CurrentLight.green
+        case .green:
+            yellowColor = yellowColor.opacity(lightIsOff)
+            greenColor = greenColor.opacity(lightIsOn)
+            currentLight = CurrentLight.red
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
--- TrafficLight/TrafficSignalView.swift
+++ TrafficLight/TrafficSignalView.swift
@@ -2,24 +2,25 @@
 //  TrafficSignalView.swift
 //  TrafficLight
 //
-//  Created by Олег Федоров on 26.10.2021.
+//  Created by Олег Федоров on 27.10.2021.
 //
 
 import SwiftUI
 
 struct TrafficSignalView: View {
     var color: Color?
-
+    var opacity: Double?
+    
     var body: some View {
         Circle()
-            .foregroundColor(color?.opacity(1))
+            .foregroundColor(color?.opacity(opacity ?? 0.2) ?? .blue)
             .frame(width: 120, height: 120)
             .overlay(Circle().stroke(Color(.white), lineWidth: 4))
-    }    
+    }
 }
 
 struct TrafficSignalView_Previews: PreviewProvider {
     static var previews: some View {
-        TrafficSignalView(color: .blue)
+        TrafficSignalView()
     }
 }
